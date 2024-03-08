91249b86e34bec10e736aae5a96b3b5cde7443a5
developer <1735721264@qq.com>
2024-03-08 20:44:40 +0800

    x

--- index.js
+++ index.js
@@ -3,5 +3,5 @@
 if (process.env.NODE_ENV === 'production') {
   module.exports = require('./build/umd.min.js');
 } else {
-  module.exports = require('./build/umd.js');
+  module.exports = require('./build/cmj.js');
 }
