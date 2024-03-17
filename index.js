38fb56bbaf3964004447d8d370311cc568c1188a
developer <1735721264@qq.com>
2024-03-17 20:31:22 +0800

    x

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./build/umd.min.js');
+  module.exports = require('./build/cmj.min.js');
 } else {
-  module.exports = require('./build/cmj.js');
+  module.exports = require('./src/index.js');
 }
